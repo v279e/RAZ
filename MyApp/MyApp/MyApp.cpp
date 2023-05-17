56b68de6f87c051c3e57e9473f65bd62406839bd
v279e <vitya.ekgardt@yandex.ru>
2023-05-17 17:05:09 +0300

    test

---
+++ MyApp/MyApp/MyApp.cpp
@@ -0,0 +1,38 @@
+﻿#include<stdio.h>  
+#include<time.h>
+#include<string.h>
+
+int main() {
+	FILE* file;
+	const size_t len = 1024;
+
+	if (fopen_s(&file, "C:/Users/Victor/Desktop/test.evv", "r+")) {
+		printf_s("Ошибка в открытие файла!");
+		getchar();
+		return 0;
+	}
+
+	char buffer[len] = { 0 };
+	fread_s(buffer, len, sizeof(char), len, file);
+	printf_s("%s\n", buffer);
+
+	time_t rawtime;
+	struct tm info;
+	time(&rawtime);
+	localtime_s(&info, &rawtime);
+
+	memset(buffer, '\0', len);
+	strftime(buffer, len, "%Y-%m-%d %H:%M:%S ", &info);
+	strcat_s(buffer, "Ekgardt Victor 201-331\n");
+
+	fseek(file, 0, 0);
+	fwrite(buffer, sizeof(char), len, file);
+
+	fclose(file);
+
+	getchar();
+
+	return 0;
+
+
+}
